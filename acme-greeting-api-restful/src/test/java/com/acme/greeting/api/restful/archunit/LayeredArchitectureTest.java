aa62101f4270959237ca46cd60236765af4ef450
vjmadrid <vjmadrid@atsistemas.com>
2020-12-20 20:43:04 +0100

    Adapt project

---
+++ acme-greeting-api-restful/src/test/java/com/acme/greeting/api/restful/archunit/LayeredArchitectureTest.java
@@ -0,0 +1,31 @@
+package com.acme.greeting.api.restful.archunit;
+
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
+
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+@AnalyzeClasses(packages = "com.acme.greeting.api.restful", 
+importOptions = { 
+		ImportOption.DoNotIncludeTests.class,
+		ImportOption.DoNotIncludeJars.class, 
+		ImportOption.DoNotIncludeArchives.class 
+	}
+)
+public class LayeredArchitectureTest {
+	
+	@ArchTest
+	public static final ArchRule layered_architecture_dependencies_are_respected = 
+	    layeredArchitecture()
+	    //Layers
+		.layer("Entity layer").definedBy("..entity..")
+		.layer("Controller layer").definedBy("..controller..")
+		.layer("Config layer").definedBy("..config..")
+		//Conditions
+		.whereLayer("Entity layer").mayOnlyBeAccessedByLayers("Controller layer")
+		.whereLayer("Controller layer").mayNotBeAccessedByAnyLayer()
+		.whereLayer("Config layer").mayNotBeAccessedByAnyLayer();
+
+}

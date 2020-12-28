0295ed101b94dbe7e55f45e9a9b26b34cad72d76
vjmadrid <vjmadrid@atsistemas.com>
2020-12-28 10:37:12 +0100

    Change liquibase

--- acme-api-greeting/src/main/resources/db/changelog/dml/liquibase-changeLog-data.sql
+++ acme-api-greeting/src/main/resources/db/changelog/dml/liquibase-changeLog-data.sql
@@ -1,3 +1,3 @@
-insert into Greeting (GreetingContent, GreetingTime) values ( 'Hello, Greeting 1!', '2020-12-01T10:30:35.990');
-insert into Greeting (GreetingContent, GreetingTime) values ( 'Hello, Greeting 2!', '2020-12-01T10:45:15.000');
-insert into Greeting (GreetingContent, GreetingTime) values ( 'Hello, Greeting 3!', '2020-12-02T14:15:40.123');
+insert into Greeting (GreetingContent, GreetingCreatedAt) values ( 'Hello, Greeting 1!', '2020-12-01T10:30:35.990');
+insert into Greeting (GreetingContent, GreetingCreatedAt) values ( 'Hello, Greeting 2!', '2020-12-01T10:45:15.000');
+insert into Greeting (GreetingContent, GreetingCreatedAt) values ( 'Hello, Greeting 3!', '2020-12-02T14:15:40.123');
